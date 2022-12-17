f642a575768c5aa1fd56bcf8e1564ffbd537a3ef
Zaur199 <zaur.rojer@mail.ru>
2022-12-17 19:21:47 +0300

    Changes added

--- ЗАДАЧА1.py
+++ ЗАДАЧА1.py
@@ -1,13 +1,16 @@
 # Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-
 # Пример:
-
 # - 6782 -> 23
 # - 0,56 -> 11
 
-x = int(input("Введите число: "))
-result = 0
-while x > 0:
-    result = x % 10 + result
-    x = x // 10
-print(result)
+def NumberSum(item):
+    result = 0
+    i = 0
+    for i in item:
+        if (i != '.') and (i != '-'):
+            result = result + int(i)
+    return result
+
+num = float(input('Введите вещественное число: '))
+item = str(num)
+print(f'Сумма:{NumberSum(item)}')
--- ЗАДАЧА5.py
+++
@@ -1,6 +0,0 @@
-# Реализуйте алгоритм перемешивания списка
-import random
-lst = [5, 8, -9, 4, -1, 0, 3]
-# Используем функцию shuffle() модуля random
-random.shuffle(lst) 
-print(lst)
